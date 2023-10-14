1ad03b77769dabae48125196400153693482f5d5
Ajey <ajeysrini@gmail.com>
2023-10-14 13:36:24 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import connectToDb from "./db_utils/mongoos-connection.js";
+import productRoutes from "./routes/productDetails.js";
+import purchaseOrderRoutes from "./routes/purchaseorder.js"
+
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+// Use your API routes
+
+app.use('/api/purchaseorders', purchaseOrderRoutes);
+
+app.use('/api/products', productRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectToDb(); // Connect to MongoDB when the app starts
+});
