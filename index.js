df52f24db19dbc90f0d6575520fa9ffa704c3a10
Ajey <ajeysrini@gmail.com>
2023-10-14 18:53:56 +0530

    cros issue fixed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectToDb from "./db_utils/mongoos-connection.js";
 import productRoutes from "./routes/productDetails.js";
 import purchaseOrderRoutes from "./routes/purchaseorder.js"
+import cors from 'cors';
 
 const app = express();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5050;
 app.use(express.static('public'));
 app.use(express.json());
 
+app.use(cors());
 // Use your API routes
 
 app.use('/api/purchaseorders', purchaseOrderRoutes);
